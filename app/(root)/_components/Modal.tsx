f957e7cd3018961f360a5ba2c88ab03d1f680110
Pseurae <adhith.chand@gmail.com>
2024-04-19 09:35:57 +0530

    [modal] Use event listeners for backdrop click

--- app/(root)/_components/Modal.tsx
+++ app/(root)/_components/Modal.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useLayoutEffect, useRef } from "react";
+
 type ModalProps = {
     children: React.ReactNode;
     isModalOpen: boolean,
@@ -5,14 +7,32 @@ type ModalProps = {
     parentStyles?: string;
 };
 
+const ModalWrapper = ({ children, closeModal, parentStyles="" }: Omit<ModalProps, 'isModalOpen'>) => {
+    const parentRef = useRef<HTMLDivElement>(null!);
+
+    const listener = useCallback((e: MouseEvent) => {
+        if (e.target == parentRef?.current)
+            closeModal();
+    }, []);
+
+    useLayoutEffect(() => {
+        setTimeout(() => window.addEventListener('click', listener));
+        return () => window.removeEventListener('click', listener);
+    }, []);
+
+    return (
+        <div ref={parentRef} className={"fixed h-screen w-full z-50 top-0 left-0 " + parentStyles}>
+            {/* <div className="w-full h-full absolute -z-10" onClick={() => closeModal()} /> */}
+            {children}
+        </div>
+    );
+}
+
 const Modal = ({ children, isModalOpen, closeModal, parentStyles = "" } : ModalProps) => {
     if (!isModalOpen) return null;
 
     return (
-        <div className={"fixed h-screen w-full z-50 top-0 left-0 " + parentStyles}>
-            <div className="w-full h-full absolute -z-10" onClick={() => closeModal()} />
-            {children}
-        </div>
+        isModalOpen ? (<ModalWrapper closeModal={closeModal} parentStyles={parentStyles}>{children}</ModalWrapper>) : null
     );
 }
 
